846c0723d9552f23a16a2ede305dc64bbc186b60
Gregory M. Green <gregorymgreen@gmail.com>
2020-10-15 11:04:41 +0200

    Added trace augmentation function that can calculate dv/dt, v^2 and |grad(v)|^2

--- scripts/flow_ffjord_tf.py
+++ scripts/flow_ffjord_tf.py
@@ -26,9 +26,93 @@ import json
 from utils import *
 
 
+from tensorflow_probability.python import math as tfp_math
+from tensorflow_probability.python.internal import prefer_static
+
+def trace_jacobian_exact_reg(ode_fn, state_shape, dtype,
+                             kinetic_reg=None, jacobian_reg=None,
+                             dv_dt_reg=None):
+    """Generates a function that computes `ode_fn` and trace of the jacobian.
+
+    Augments provided `ode_fn` with explicit computation of the trace of the
+    jacobian. This approach scales quadratically with the number of dimensions.
+    This method computes unreduced trace, as reduction is performed inside of the
+    bijector class.
+
+    Optionally, regularization terms are subtracted from the trace, penalizing
+    various measures of the transformation.
+
+    Args:
+      ode_fn: `Callable(time, state)` that computes time derivative.
+      state_shape: `TensorShape` representing the shape of the state.
+      dtype: `tf.DType` object representing the dtype of `state` tensor.
+      dv_dt_reg: `float` indicating how strongly to penalize |dv/dt|^2.
+      kinetic_reg: `float` indicating how strongly to penalize |v|^2.
+      jacobian_reg: `float` indicating how strongly to penalize |grad(v)|^2.
+
+    Returns:
+      augmented_ode_fn: `Callable(time, (state, log_det_jac))` that computes
+        augmented time derivative `(state_time_derivative, trace_estimation)`.
+    """
+    del state_shape, dtype  # Not used by trace_jacobian_exact
+
+    def augmented_ode_fn(time, state_log_det_jac):
+        """Computes both time derivative and trace of the jacobian."""
+        state, _ = state_log_det_jac
+        ode_fn_with_time = lambda x: ode_fn(time, x)
+        batch_shape = [prefer_static.size0(state)]
+
+        if dv_dt_reg is not None:
+            watched_vars = [time, state]
+        elif (kinetic_reg is not None) or (jacobian_reg is not None):
+            watched_vars = [state]
+        else:
+            watched_vars = []
+
+        with tf.GradientTape(watch_accessed_variables=False,
+                             persistent=True) as g:
+          #g.watch([time, state])
+          g.watch(watched_vars)
+          state_time_derivative, diag_jac = tfp_math.diag_jacobian(
+              xs=state, fn=ode_fn_with_time, sample_shape=batch_shape)
+          # tfp_math.diag_jacobian returns lists
+          if isinstance(state_time_derivative, list):
+              state_time_derivative = state_time_derivative[0]
+          if isinstance(diag_jac, list):
+              diag_jac = diag_jac[0]
+
+        trace_value = diag_jac
+
+        # Calculate regularization terms
+        if (dv_dt_reg is not None) or (jacobian_reg is not None):
+            delv_delx = g.batch_jacobian(state_time_derivative, state)
+
+        if dv_dt_reg is not None:
+            delv_delt = g.gradient(state_time_derivative, time)
+            vnabla_v = tf.linalg.matvec(delv_delx, state_time_derivative)
+            dv_dt = delv_delt + vnabla_v
+            #print('dv/dt :', dv_dt)
+            trace_value = trace_value - dv_dt_reg * dv_dt**2
+
+        if kinetic_reg is not None:
+            #print('v :', state_time_derivative.shape)
+            trace_value = trace_value - kinetic_reg * state_time_derivative**2
+
+        if jacobian_reg is not None:
+            jacobian_norm2 = tf.math.reduce_sum(delv_delx**2, axis=-1)
+            #print('|J|^2 :', jacobian_norm2.shape)
+            trace_value = trace_value - jacobian_reg * jacobian_norm2
+
+        return state_time_derivative, trace_value
+
+    return augmented_ode_fn
+
+
 class ForceFieldModel(snt.Module):
     def __init__(self, n_dim, n_hidden, hidden_size, name='dz_dt'):
         super(ForceFieldModel, self).__init__(name=name)
+
+        self._n_dim = n_dim
 
         output_sizes = [hidden_size] * n_hidden + [n_dim]
         self._nn = snt.nets.MLP(
@@ -38,36 +122,68 @@ class ForceFieldModel(snt.Module):
         )
 
     def __call__(self, t, x):
+        """
+        Returns the vector dx/dt.
+
+        Inputs:
+          t (tf.Tensor): Scalar representing time.
+          x (tf.Tensor): Spatial coordinates at which to evaluate
+            dx/dt. Shape = (n_points, n_dim).
+        """
         # Concatenate time and position vectors
         tx = tf.concat([tf.broadcast_to(t, [x.shape[0],1]), x], 1)
         # Return dz_dt(t,x)
         return self._nn(tx)
 
+    def augmented_field(self, t, y):
+        """
+        Returns the vector dy/dt, where y = (x, s), and s is the
+        path length. This is useful when regularizing a neural ODE
+        by path length.
+
+        Inputs:
+          t (tf.Tensor): Scalar representing time.
+          y (tf.Tensor): Concatenation of spatial coordinates and path
+            length. Shape = (n_points, n_dim+1).
+        """
+        x,s = tf.split(y, [self._n_dim,1], axis=1)
+        dx_dt = self.__call__(t, x)
+        ds_dt = tf.math.sqrt(tf.math.reduce_sum(dx_dt**2, axis=1))
+        dy_dt = tf.concat([dx_dt, tf.expand_dims(ds_dt, 1)], 1)
+        return dy_dt
+
 
 class FFJORDFlow(tfd.TransformedDistribution):
     def __init__(self, n_dim, n_hidden, hidden_size,
-                 exact=True, atol=1.e-5, name='DF'):
+                 reg_kw=dict(), atol=1.e-5, name='DF'):
         self._n_dim = n_dim
         self._n_hidden = n_hidden
         self._hidden_size = hidden_size
         self._name = name
 
         # Force field guiding transformation
-        dz_dt = ForceFieldModel(n_dim, n_hidden, hidden_size)
+        self.dz_dt = ForceFieldModel(n_dim, n_hidden, hidden_size)
 
-        ode_solver = tfp.math.ode.DormandPrince(atol=atol)
+        self.ode_solver = tfp.math.ode.DormandPrince(atol=atol)
 
-        if exact:
-            trace_augmentation_fn = tfb.ffjord.trace_jacobian_exact
+        #if exact:
+        #    trace_augmentation_fn = tfb.ffjord.trace_jacobian_exact
+        #else:
+        #    # Stochastic estimate of the Jacobian. Better scaling with
+        #    # number of dimensions, but noisy.
+        #    trace_augmentation_fn = tfb.ffjord.trace_jacobian_hutchinson
+        
+        if len(reg_kw):
+            print('Using regularization.')
+            def trace_augmentation_fn(*args):
+                return trace_jacobian_exact_reg(*args, **reg_kw)
         else:
-            # Stochastic estimate of the Jacobian. Better scaling with
-            # number of dimensions, but noisy.
-            trace_augmentation_fn = tfb.ffjord.trace_jacobian_hutchinson
+            trace_augmentation_fn = tfb.ffjord.trace_jacobian_exact
 
         # Initialize bijector
         bij = tfb.FFJORD(
-            state_time_derivative_fn=dz_dt,
-            ode_solve_fn=ode_solver.solve,
+            state_time_derivative_fn=self.dz_dt,
+            ode_solve_fn=self.ode_solver.solve,
             trace_augmentation_fn=trace_augmentation_fn
         )
 
@@ -88,6 +204,33 @@ class FFJORDFlow(tfd.TransformedDistribution):
 
         self.n_var = sum([int(tf.size(v)) for v in self.trainable_variables])
         print(f'# of trainable variables: {self.n_var}')
+
+    def calc_path_length(self, x, forward=True):
+        if forward:
+            dxs_dt = self.dz_dt.augmented_field
+        else:
+            dxs_dt = lambda t,x: -self.dz_dt.augmented_field(1.-t,x)
+
+        # Concatenate the initial path length (0) to x
+        xs0 = tf.concat([x, tf.zeros([self._n_dim,1])], 1)
+
+        # Solve the trajectory of x, keeping track of the path length
+        res = self.ode_solver.solve(
+            self.dz_dt.augmented_field,
+            0., xs0,
+            solution_times=[1.]
+        )
+
+        # Extract the final position and the path length
+        xs_T = tf.squeeze(res.states, axis=0)
+        x_T,s_T = tf.split(xs_T, [self._n_dim,1], axis=1)
+
+        return x_T, s_T
+
+    def calc_trajectories(self, n_samples, t_eval):
+        x0 = self.distribution.sample([n_samples])
+        res = self.ode_solver.solve(self.dz_dt, 0, x0, t_eval)
+        return res
     
     def save(self, fname_base):
         # Save variables
@@ -161,7 +304,7 @@ def train_flow(flow, data,
         lr_schedule = keras.optimizers.schedules.ExponentialDecay(
             2.e-2,
             n_steps,
-            0.005,
+            0.05,
             staircase=False
         )
         opt = tfa.optimizers.RectifiedAdam(
@@ -203,8 +346,8 @@ def train_flow(flow, data,
             loss = -tf.reduce_mean(flow.log_prob(batch))
         grads = g.gradient(loss, variables)
         #tf.print([(v.name,tf.norm(v)) for v in grads])
-        grads,global_norm = tf.clip_by_global_norm(grads, 1.)
-        #tf.print('\n',global_norm)
+        grads,global_norm = tf.clip_by_global_norm(grads, 10.)
+        #tf.print('\nglobal_norm =', global_norm)
         #tf.print([(v.name,tf.norm(v)) for v in grads])
         opt.apply_gradients(zip(grads, variables))
         return loss
@@ -270,10 +413,9 @@ def calc_f_gradients(f, eta):
 
 
 def main():
-    #flow = create_flow(2, 4, 16)
     flow = FFJORDFlow(2, 4, 16)
 
-    n_samples = 32*1024
+    n_samples = 8*1024
     mu = [[-2., 0.], [2., 0.]]
     cov = [
         [[1., 0.],
